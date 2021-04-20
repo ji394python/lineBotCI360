cb5e6f39088976513abcd0e4b2c238b08e0940ad
小天 <60791108+ji394python@users.noreply.github.com>
2021-04-20 11:46:55 +0800

    delete userid

--- app.py
+++ app.py
@@ -34,10 +34,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     get_message = event.message.text
-    get_userId = event.source.user_id
-
     # Send To Line
-    reply = TextSendMessage(text=f"[九官鳥如我] {get_message} \n {get_userId}")
+    reply = TextSendMessage(text=f"[九官鳥如我] {get_message}")
     line_bot_api.reply_message(event.reply_token, reply)
 
 # 處理訊息:加入
